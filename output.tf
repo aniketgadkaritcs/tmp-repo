733cbd1f046787c01882f4cdc9b5b1c93d3add37
aniketgadkari <40064400+aniketgadkari@users.noreply.github.com>
2024-03-03 16:03:34 -0500

    commit

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
-/*
+
 output "endpoint" {
     #value = aws_vpc_endpoint.ec2.service_name
-    value = [for endpoint in aws_aws_vpc_endpoint.endpoint: endpoint.service_name ]
+    value = [for endpoint in aws_vpc_endpoint.endpoint: "This is the endpoint name ${endpoint.service_name}" ]
 }
 
 output "endpoint_id" {
   #value = aws_vpc_endpoint.ec2.id
-  value = [for endpoint in aws_aws_vpc_endpoint.endpoint: endpoint.id ]
+  value = [for endpoint in aws_vpc_endpoint.endpoint: "This is the endpoint id ${endpoint.id}" ]
 }
-*/
+
 /*
 output "endpoint_sg" {
     value = aws_security_group.epsg.id  
