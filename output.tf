4f90797b30f4f071a76698cac74aacf50094de60
aniketgadkari <40064400+aniketgadkari@users.noreply.github.com>
2024-03-03 14:24:31 -0500

    c

--- output.tf
+++ output.tf
@@ -1,6 +1,10 @@
 output "endpoint" {
     value = aws_vpc_endpoint.ec2.service_name
 }
+
+output "endpoint_id" {
+  value = aws_vpc_endpoint.ec2.id
+}
 /*
 output "endpoint_sg" {
     value = aws_security_group.epsg.id  
