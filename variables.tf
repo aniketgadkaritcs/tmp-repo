4479f28375ae6681409c409367ebfad2b46be8cf
aniketgadkari <40064400+aniketgadkari@users.noreply.github.com>
2024-03-03 14:13:17 -0500

    commit

--- variables.tf
+++ variables.tf
@@ -3,8 +3,10 @@ variable "vpcid" {
     default = "vpc-0684439dd827d81b9"
 }
 
-variable "sg_id" {
+/*variable "sg_id" {
 }
+*/
+
 variable "cidr" {
     type = string
     default = "10.10.10.0/24"
