0de5a195d405374840a95ddeede6cdc1362fd9c9
aniketgadkari <40064400+aniketgadkari@users.noreply.github.com>
2024-03-03 15:48:32 -0500

    commit

--- endpoint.tf
+++ endpoint.tf
@@ -1,26 +1,32 @@
-resource "aws_vpc_endpoint" "ec2" {
+resource "aws_vpc_endpoint" "endpoint" {
+  for_each = toset([var.endpoints])
   vpc_endpoint_type  = "Interface"
   vpc_id             = var.vpcid
-  service_name       = "com.amazonaws.us-east-1.ec2"
+  #service_name       = "com.amazonaws.us-east-1.ec2"
+  service_name       = "com.amazonaws.us-east-1.${each.key}"
   #service_name       = "com.amazonaws.us-east-1.${file("../endpoints.txt")}"
-  security_group_ids = [aws_security_group.epsg.id]
+  security_group_ids = [aws_security_group.epsg[each.key].id]
   tags = {
     Name = "ecvpcendpoint"
   }
+  
   depends_on = [ aws_security_group.epsg, aws_security_group_rule.ep_sg_rule ]
 
 }
 
 resource "aws_security_group" "epsg" {
-  name   = "endpoint_sg"
+  for_each = toset([var.endpoints])
+  name = "$endpoint_sg_${each.key}"
+  #name   = "endpoint_sg"
   vpc_id = var.vpcid
 }
 
 resource "aws_security_group_rule" "ep_sg_rule" {
+  for_each = toset([var.endpoints])
   type = "ingress"
   from_port = "443"
   to_port = "443"
   protocol = "tcp"
   cidr_blocks = [var.cidr]
-  security_group_id = aws_security_group.epsg.id
+  security_group_id = aws_security_group.epsg[each.key].id
 }
--- output.tf
+++ output.tf
@@ -1,10 +1,14 @@
+/*
 output "endpoint" {
-    value = aws_vpc_endpoint.ec2.service_name
+    #value = aws_vpc_endpoint.ec2.service_name
+    value = [for endpoint in aws_aws_vpc_endpoint.endpoint: endpoint.service_name ]
 }
 
 output "endpoint_id" {
-  value = aws_vpc_endpoint.ec2.id
+  #value = aws_vpc_endpoint.ec2.id
+  value = [for endpoint in aws_aws_vpc_endpoint.endpoint: endpoint.id ]
 }
+*/
 /*
 output "endpoint_sg" {
     value = aws_security_group.epsg.id  
